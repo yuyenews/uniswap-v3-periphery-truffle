517956d1583f47425dea9bf6ad4724e9e42f497c
yuyenews <yuyemail@163.com>
2021-09-01 23:38:09 +0800

    init commit

--- contracts/NonfungibleTokenPositionDescriptor.sol
+++ contracts/NonfungibleTokenPositionDescriptor.sol
@@ -3,7 +3,7 @@ pragma solidity =0.7.6;
 pragma abicoder v2;
 
 import "@uniswap-truffle/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
-import "@uniswap-truffle/libs/contracts/libraries/SafeERC20Namer.sol";
+import "@uniswap-truffle/lib/contracts/libraries/SafeERC20Namer.sol";
 
 import "./libraries/ChainId.sol";
 import "./interfaces/INonfungiblePositionManager.sol";
