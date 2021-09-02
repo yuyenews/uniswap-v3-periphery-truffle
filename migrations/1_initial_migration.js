c6635b852f82bffc590fc7d4ffe595f588136ff8
yuyenews <yuyemail@163.com>
2021-09-02 21:11:21 +0800

    init commit

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const SwapRouter = artifacts.require("SwapRouter");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(SwapRouter, "factoryAddress", "WETH9");
 };
